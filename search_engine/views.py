6c4b503cff81602f8783f6a5f65be449931d5bb0
YoungkiKim91 <bomulcisu@tamu.edu>
2023-04-23 12:12:52 -0500

    homepage screen draft

---
+++ search_engine/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+def welcome(request):
+    return render(request, 'index.html') 
+
+def result(request):
+    index = request.GET['index']
+    print("Fix here for retrieve the photo")
+    return render(request, 'result.html')
