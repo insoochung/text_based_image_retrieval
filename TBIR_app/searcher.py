d24742bc96956798edbe5013b91460b4d7730bf0
Insoo Chung <insoooo.chung@gmail.com>
2023-04-29 14:32:39 -0500

    Delete tests

--- TBIR_app/searcher.py
+++ TBIR_app/searcher.py
@@ -1,27 +1,34 @@
+import sys
+import os
+from pathlib import Path
+sys.path.insert(0, str(Path(__file__).parent.parent))  # nopep8
+
+import django
 import numpy as np
-from TBIR_app.models import Photo
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
+django.setup()
+
+from TBIR_app.models import Photo  # nopep8
 
 
 class Searcher:
     def __init__(self):
-        pass
-        # self.caption_embedding = self.initialize_caption_embedding()
-        # self.caption_embedding = self.initialize_caption_embedding()
+        self.caption_embedding = self.initialize_caption_embedding()
 
     def initialize_caption_embedding(self):
-        return []
-        # embedding = []
-        # for photo in Photo.objects.all():
-        #     vec = photo.caption_vector
-        #     embedding.append(np.array(vec))
+        embedding = []
+        for photo in Photo.objects.all():
+            vec = photo.caption_vector
+            print(vec)
+            embedding.append(np.array(vec))
 
-        # return np.stack(embedding)
+        return np.stack(embedding)
 
     def initialize_face_embedding(self):
         return []
-        # embedding = []
-        # for photo in Photo.objects.all():
-        #     vec = photo.name_vector
-        #     embedding.append(np.array(vec))
 
-        # return np.stack(embedding)
+
+if __name__ == "__main__":
+    searcher = Searcher()
+    searcher.initialize_caption_embedding()
--- TBIR_app/tests.py
+++ TBIR_app/tests.py
@@ -1,9 +1,1 @@
 from django.test import TestCase
-from TBIR_app.searcher import Searcher
-# python manage.py test
-
-
-class SearcherTests(TestCase):
-    def test_caption_embedding(self):
-        searcher = Searcher()
-        self.assertEqual(searcher.initialize_caption_embedding(), [])
