b493390673fbf96c2c91eae24f88dfac31eaa245
Insoo Chung <insoooo.chung@gmail.com>
2023-04-29 21:02:36 -0500

    Link searcher with view templates

--- TBIR_app/searcher.py
+++ TBIR_app/searcher.py
@@ -11,7 +11,13 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
 django.setup()
 
 from TBIR_app.models import Photo  # nopep8
-from dl_modules.text_vectorizer import TextVectorizer  # nopep8
+from TBIR_app.dl_modules.text_vectorizer import TextVectorizer  # nopep8
+
+
+def standardize(scores):
+    _max = np.max(scores)
+    _min = np.min(scores)
+    return (scores - _min) / (_max - _min)  # force min to 0 and max to 1
 
 
 class Searcher:
@@ -21,42 +27,74 @@ class Searcher:
 
     def initialize_caption_embedding(self):
         embedding = []
+        ids = []
         for photo in Photo.objects.all():
             caption_vector = np.array(photo.caption_vector)
             embedding.append(caption_vector)
+            ids.append(photo.id)
+
+        assert ids == list(range(1, len(Photo.objects.all()) + 1))
         embedding = np.array(embedding)
 
         return embedding
 
     def initialize_face_embedding(self):
         embedding = []
+        ids = []
         for photo in Photo.objects.all():
             name_vector = np.array(photo.name_vector)
             embedding.append(name_vector)
-        embedding = np.array(embedding)
+            ids.append(photo.id)
 
+        assert ids == list(range(1, len(Photo.objects.all()) + 1))
+        embedding = np.array(embedding)
         return embedding
 
-    def query_caption_score(self, query="two men under sky"):
+    def query_caption_score(self, query="two men under sky", metric="cos_sim"):
         # retrive model
         model = TextVectorizer()
 
         # captions
-        caption_for_allimgs = self.caption_embedding
-
+        caption_embedding = self.caption_embedding  # (N,256)
+        # print(caption_embedding[:3])
         query_embedding = model(query)  # get embedding for query
+        # print(query_embedding)
+        # assert 0
+        query_embedding = query_embedding.reshape((1, -1))  # (1,256)
+
+        if metric == "cos_sim":
+            cosine = cosine_similarity(
+                caption_embedding, query_embedding.reshape(1, -1))
+            # reshape cosine similarity array to (1503, 1)
+            scores = cosine.reshape(-1, 1)
+        else:
+            scores = np.power(caption_embedding -
+                              query_embedding, 2)  # (N, 256)
+            scores = np.sum(scores, axis=1)  # (N)
+            scores = standardize(scores)
+            scores = 1 - scores
+            scores = scores.reshape((-1, 1))  # (N, 1)
+        return scores
+        # assert 0
+        # assert 0
+        # print(scores.shape)
+        # assert 0
+        # assert 0, (caption_embedding.shape, query_embedding.shape)
+
         # calculate cosine similarity between caption embeddings and query embedding
-        cosine = cosine_similarity(
-            caption_for_allimgs, query_embedding.reshape(1, -1))
-        # reshape cosine similarity array to (1503, 1)
-        cosine = cosine.reshape(-1, 1)
+        # cosine = cosine_similarity(
+        #     caption_embedding, query_embedding.reshape(1, -1))
+        # # reshape cosine similarity array to (1503, 1)
+        # cosine = cosine.reshape(-1, 1)
+        return scores
 
-        # get the top 25 cosine similarity indices
-        top_25_cosine_indices = np.argsort(cosine, axis=0)[-25:].flatten()
-        # get the top 25 cosine similarity values
-        top_25_cosine_values = cosine.take(top_25_cosine_indices)
+        # # get the top 25 cosine similarity indices
+        # top_25_cosine_indices = reversed(
+        #     np.argsort(cosine, axis=0)[-25:].flatten())
+        # # get the top 25 cosine similarity values
+        # top_25_cosine_values = cosine.take(top_25_cosine_indices)
 
-        return list(zip(top_25_cosine_indices, top_25_cosine_values))
+        # return list(zip(top_25_cosine_indices, top_25_cosine_values))
 
     def query_face_score(self, query="insoo and jinhyun"):
 
@@ -86,14 +124,34 @@ class Searcher:
         union_count = np.sum(union, axis=1)
 
         iou = intersection_count / union_count
-        iou = iou.reshape(-1, 1)  # reshape to (1503, 1)
+        iou = iou.reshape(-1, 1)  # reshape to (1503)
+        return iou
+        # # get the top 25 cosine similarity indices
+        # top_25_iou_indices = reversed(np.argsort(iou, axis=0)[-25:].flatten())
+        # # get the top 25 cosine similarity values
+        # top_25_iou_values = iou.take(top_25_iou_indices)
 
-        # get the top 25 cosine similarity indices
-        top_25_iou_indices = np.argsort(iou, axis=0)[-25:].flatten()
+        # return list(zip(top_25_iou_indices, top_25_iou_values))
+
+    def query(self, query, caption_ratio=1.0, face_tags_ratio=0.2, top_k=25):
+        scores = np.zeros(shape=(len(Photo.objects.all()), 1))
+        if caption_ratio > 0:
+            caption_scores = self.query_caption_score(query)  # 0~1
+            # scores += standardize(caption_scores) * caption_ratio
+            # print(caption_scores)
+            scores += standardize(caption_scores) * caption_ratio
+            # print(scores)
+            # assert 0
+        if face_tags_ratio > 0:
+            face_scores = self.query_face_score(query)  # 0~1
+            scores += standardize(face_scores) * face_tags_ratio
+        scores = np.reshape(scores, (-1, 1))
+        top_k_scores_indices = list(reversed(
+            np.argsort(scores, axis=0)[-top_k:].flatten()))
         # get the top 25 cosine similarity values
-        top_25_iou_values = iou.take(top_25_iou_indices)
+        top_k_scores_values = scores.take(top_k_scores_indices)
 
-        return list(zip(top_25_iou_indices, top_25_iou_values))
+        return list(zip(top_k_scores_indices, top_k_scores_values))
 
 
 if __name__ == "__main__":
--- TBIR_app/views.py
+++ TBIR_app/views.py
@@ -1,4 +1,23 @@
+import os
+import boto3
 from django.shortcuts import render
+from TBIR_app.searcher import Searcher
+from TBIR_app.models import Photo
+
+from django_project.settings import AWS_STORAGE_BUCKET_NAME, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_S3_REGION_NAME
+
+SEARCHER = Searcher()
+S3_CLIENT = boto3.client("s3", region_name=AWS_S3_REGION_NAME,
+                         aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+
+
+def maybe_download(s3_client, photo, images_dir, bucket_name):
+    image_name = photo.image_url.split("/")[-1]
+    image_path = f"{images_dir}/{image_name}"
+    if not os.path.exists(image_path):
+        s3_client.download_file(
+            bucket_name, image_name, image_path)
+    return image_path
 
 
 def index(request):
@@ -6,4 +25,22 @@ def index(request):
 
 
 def result(request):
-    return render(request, 'result.html')
+    query = request.GET['index']
+    search_res = SEARCHER.query(query, face_tags_ratio=0.2)  # id, score
+    print(search_res)
+    print(type(search_res))
+    for i, score in search_res:
+        print(Photo.objects.get(id=int(i + 1)))
+        print(score)
+    # assert 0
+    photos = [(Photo.objects.get(id=int(i + 1)), score)
+              for (i, score) in search_res]
+    photos_processed = []
+    for photo, score in photos:
+        maybe_download(
+            S3_CLIENT, photo, f"{os.path.dirname(__file__)}/../static/images", AWS_STORAGE_BUCKET_NAME)
+        print(photo.image_url)
+        photos_processed.append(
+            (f"static/images/{photo.image_url.split('/')[-1]}", score))
+
+    return render(request, 'result.html', {'photos': photos_processed})
